cbc9d10f753dab7af9b56504e3534da8747abc72
Mark Haugen <mhaugen@alarm.com>
2023-11-13 10:27:34 -0500

    Make everything TS

--- tests/test-helper.ts
+++ tests/test-helper.ts
@@ -5,6 +5,8 @@ import { setApplication } from '@ember/test-helpers';
 import { setup } from 'qunit-dom';
 import { start } from 'ember-qunit';
 
+import 'ember-qunit';
+
 setApplication(Application.create(config.APP));
 
 setup(QUnit.assert);
